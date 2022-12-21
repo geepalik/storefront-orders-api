1392892c479fd36aec3d9dee009cd6ade839cbf1
gilpal <gil.palikaras@gmail.com>
2022-12-21 17:19:03 +0200

    when order is created, if coupons passed are not allowed in storefront, throw error

--- src/order/order.dao.ts
+++ src/order/order.dao.ts
@@ -8,6 +8,9 @@ export class OrderDao {
     private readonly orders: Order[] = [];
 
     createOrder(orderDto: OrderDto, storefrontData: Storefront): Order{
+        if(!this.checkIfOrderCouponsAllowed(storefrontData.couponCodes, orderDto.couponCodes)){
+            throw new Error("Order contains coupons that are not allowed for this storefront");
+        }
         const orderId: number = this.orders.length + 1;
         const {customerInfoName, customerInfoAdress, couponCodes} = orderDto;
         const order: Order = new Order();
@@ -19,5 +22,9 @@ export class OrderDao {
         order.associatedStorefront = storefrontData;
         order.couponCodes = couponCodes;
         return order;
+    }
+
+    private checkIfOrderCouponsAllowed(storefrontCoupons: string[], orderCoupons: string[]): boolean{
+        return orderCoupons.every(coupon => storefrontCoupons.includes(coupon));
     }
 }
